830134c9aec98c365c0d53392c7df2874126808f
Stephan Badragan <stephan.badragan@everbridge.com>
2026-02-15 19:05:11 -0800

    toggle without minimize

--- extension.js
+++ extension.js
@@ -8,191 +8,209 @@ const MAX_NUMBER = 20;
 
 export default class HappyAppyHotkeyExtension extends Extension {
 
-    enable() {
-        this.apps = [];
-        this.settings = this.getSettings('org.gnome.shell.extensions.wunder-app-hotkey');
-        this.settingId = this.settings.connect('changed', () => this.initSettings());
-        this.initSettings();
-        this.tracker = Shell.WindowTracker.get_default();
+  enable() {
+    this.apps = [];
+    this.settings = this.getSettings('org.gnome.shell.extensions.wunder-app-hotkey');
+    this.settingId = this.settings.connect('changed', () => this.initSettings());
+    this.initSettings();
+    this.tracker = Shell.WindowTracker.get_default();
 
-        for (let i = 0; i < MAX_NUMBER; i++) {
-            this.addKeyBinding(i, () => this.focusOrLaunch(this.apps[i]));
-        }
-        this.addKeyBinding('unbound-cycle', () => this.unboundCycle());
+    for (let i = 0; i < MAX_NUMBER; i++) {
+      this.addKeyBinding(i, () => this.focusOrLaunch(this.apps[i]));
+    }
+    this.addKeyBinding('unbound-cycle', () => this.unboundCycle());
+  }
+
+  disable() {
+    this.removeKeyBinding('unbound-cycle');
+    for (let i = 0; i < MAX_NUMBER; i++) {
+      this.removeKeyBinding(i);
+    }
+    if (this.settingId) {
+      this.settings.disconnect(this.settingId);
+    }
+    this.tracker = null;
+    this.settings = null;
+    this.apps = null;
+  }
+
+  initSettings() {
+    const existingApps = Gio.AppInfo.get_all()
+      .filter(ai => ai.should_show());
+
+    for (let i = 0; i < MAX_NUMBER; i++) {
+      this.apps[i] = [
+        existingApps.find(a => this.isMatchingApp(a, this.settings.get_string(`app-${i}`))),
+        this.settings.get_boolean(`start-${i}`)
+      ];
+    }
+  }
+
+  addKeyBinding(hotkey, callback) {
+    Main.wm.addKeybinding(
+      `hotkey-${hotkey}`,
+      this.settings,
+      Meta.KeyBindingFlags.NONE,
+      Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW,
+      callback
+    );
+  }
+
+  removeKeyBinding(hotkey) {
+    Main.wm.removeKeybinding(`hotkey-${hotkey}`);
+  }
+
+  isMatchingApp(app, name) {
+    return app && app.get_name() && name && app.get_name().toLowerCase() === name.toLowerCase();
+  }
+
+  focusOrLaunch(tuple) {
+    if (!tuple) {
+      return;
     }
 
-    disable() {
-        this.removeKeyBinding('unbound-cycle');
-        for (let i = 0; i < MAX_NUMBER; i++) {
-            this.removeKeyBinding(i);
-        }
-        if (this.settingId) {
-            this.settings.disconnect(this.settingId);
-        }
-        this.tracker = null;
-        this.settings = null;
-        this.apps = null;
+    const definedApp = tuple[0];
+    const shouldLaunch = tuple[1];
+    if (!definedApp) {
+      return;
     }
 
-    initSettings() {
-        const existingApps = Gio.AppInfo.get_all()
-            .filter(ai => ai.should_show());
+    const appWindows = [];
+    let activeAppWindow = null;
+    let topmostAppWindow = null;
+    let mostRecentTime = 0;
+    let overall_first_time = 0;
+    let overall_second_time = 0;
+    let overall_first_win = null;
+    let overall_second_win = null;
 
-        for (let i = 0; i < MAX_NUMBER; i++) {
-            this.apps[i] = [
-                existingApps.find(a => this.isMatchingApp(a, this.settings.get_string(`app-${i}`))),
-                this.settings.get_boolean(`start-${i}`)
-            ];
+    const wins = this.getAllWindows();
+    for (let i = 0; i <= wins.length; i++) {
+      const mw = wins[i] && wins[i].get_meta_window();
+      if (mw) {
+        const winApp = this.tracker.get_window_app(mw);
+        const userTime = mw.get_user_time();
+
+        if (winApp.get_id() === definedApp.get_id()) {
+          appWindows.push(mw);
+
+          // The app is already active; prepare for cycling
+          if (mw.has_focus()) {
+            activeAppWindow = mw;
+          }
+
+          // Determine which window was used last
+          if (userTime > mostRecentTime) {
+            mostRecentTime = userTime;
+            topmostAppWindow = mw;
+          }
         }
+
+        if (userTime > overall_first_time) {
+          overall_second_time = overall_first_time
+          overall_second_win = overall_first_win
+
+          overall_first_time = userTime
+          overall_first_win = mw
+        } else if (userTime < overall_first_time && userTime > overall_second_time) {
+          overall_second_time = userTime
+          overall_second_time = mw
+        }
+      }
     }
 
-    addKeyBinding(hotkey, callback) {
-        Main.wm.addKeybinding(
-            `hotkey-${hotkey}`,
-            this.settings,
-            Meta.KeyBindingFlags.NONE,
-            Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW,
-            callback
-        );
-    }
-
-    removeKeyBinding(hotkey) {
-        Main.wm.removeKeybinding(`hotkey-${hotkey}`);
-    }
-
-    isMatchingApp(app, name) {
-        return app?.get_name() && name && app.get_name().toLowerCase() === name.toLowerCase();
-    }
-
-    focusOrLaunch(tuple) {
-        if (!tuple) {
-            return;
-        }
-
-        const definedApp = tuple[0];
-        const shouldLaunch = tuple[1];
-        if (!definedApp) {
-            return;
-        }
-
-        const appWindows = [];
-        let activeAppWindow = null;
-        let topmostAppWindow = null;
-        let mostRecentTime = 0;
-
-        const wins = this.getAllWindows();
-        for (let i = 0; i <= wins.length; i++) {
-            const mw = wins[i]?.get_meta_window();
-            if (mw) {
-                const winApp = this.tracker.get_window_app(mw);
-                if (winApp.get_id() === definedApp.get_id()) {
-                    appWindows.push(mw);
-
-                    // The app is already active; prepare for cycling
-                    if (mw.has_focus()) {
-                        activeAppWindow = mw;
-                    }
-
-                    // Determine which window was used last
-                    const userTime = mw.get_user_time();
-                    if (userTime > mostRecentTime) {
-                        mostRecentTime = userTime;
-                        topmostAppWindow = mw;
-                    }
-                }
-            }
-        }
-
-        if (appWindows.length > 0) {
-            if (activeAppWindow) {
-                // App was already active; cycle through its windows
-                if (appWindows.length === 1 && this.settings.get_boolean('hide-active')) {
-                    this.hide(activeAppWindow);
-                }
-                else {
-                    const currentIndex = appWindows.indexOf(activeAppWindow);
-                    const nextIndex = (currentIndex + 1) % appWindows.length;
-                    this.activate(appWindows[nextIndex]);
-                }
-            }
-            else {
-                // App wasn't active already; activate most recently used
-                this.activate(topmostAppWindow);
-            }
-            return;
-        }
-
-        if (shouldLaunch) {
-            definedApp.launch([], null);
-        }
-    }
-
-    unboundCycle() {
-        const activeWin = this.getActiveWindow();
-        const wins = this.getAllWindows();
-        let position = -1;
-
-        if (activeWin) {
-            for (let i = 0; i <= wins.length; i++) {
-                const win = wins[i]?.get_meta_window();
-                if (win === activeWin) {
-                    position = i;
-                }
-            }
-        }
-
-        for (let i = 0; i < wins.length; i++) {
-            const x = (i + position + 1) % wins.length;
-            const win = wins[x].get_meta_window();
-            if (win) {
-                const winApp = this.tracker.get_window_app(win);
-                if (!this.appIsBound(winApp)) {
-                    this.activate(win);
-                    break;
-                }
-            }
-        }
-    }
-
-    getAllWindows() {
-        const wins = global.get_window_actors()
-            .filter(wa => !wa.get_meta_window().is_override_redirect());
-
-        if (this.settings.get_boolean('restrict-to-current-workspace')) {
-            const workspace = global.get_workspace_manager().get_active_workspace().index();
-            return wins.filter(wa => wa.get_meta_window().get_workspace().index() === workspace);
+    if (appWindows.length > 0) {
+      if (activeAppWindow) {
+        // App was already active; cycle through its windows
+        if (appWindows.length == 1 && this.settings.get_boolean('hide-active') && overall_second_win) {
+          // this.hide(activeAppWindow);
+          // activeAppWindow.lower()
+          this.activate(overall_second_win)
         }
         else {
-            return wins;
+          const currentIndex = appWindows.indexOf(activeAppWindow);
+          const nextIndex = (currentIndex + 1) % appWindows.length;
+          this.activate(appWindows[nextIndex]);
         }
+      }
+      else {
+        // App wasn't active already; activate most recently used
+        this.activate(topmostAppWindow);
+      }
+      return;
     }
 
-    getActiveWindow() {
-        const win = global.display.focus_window;
+    if (shouldLaunch) {
+      definedApp.launch([], null);
+    }
+  }
 
-        if (win && win.get_window_type() !== Meta.WindowType.DESKTOP) {
-            return win;
+  unboundCycle() {
+    const activeWin = this.getActiveWindow();
+    const wins = this.getAllWindows();
+    let position = -1;
+
+    if (activeWin) {
+      for (let i = 0; i <= wins.length; i++) {
+        const win = wins[i] && wins[i].get_meta_window();
+        if (win === activeWin) {
+          position = i;
         }
-
-        return null;
+      }
     }
 
-    appIsBound(app) {
-        for (const a of this.apps) {
-            if (a[0] && app.get_id() === a[0].get_id()) {
-                return true;
-            }
+    for (let i = 0; i < wins.length; i++) {
+      const x = (i + position + 1) % wins.length;
+      const win = wins[x].get_meta_window();
+      if (win) {
+        const winApp = this.tracker.get_window_app(win);
+        if (!this.appIsBound(winApp)) {
+          this.activate(win);
+          break;
         }
-        return false;
+      }
+    }
+  }
+
+  getAllWindows() {
+    const wins = global.get_window_actors()
+      .filter(wa => !wa.get_meta_window().is_override_redirect());
+
+    if (this.settings.get_boolean('restrict-to-current-workspace')) {
+      const workspace = global.get_workspace_manager().get_active_workspace().index();
+      return wins.filter(wa => wa.get_meta_window().get_workspace().index() == workspace);
+    }
+    else {
+      return wins;
+    }
+  }
+
+  getActiveWindow() {
+    const win = global.display.focus_window;
+
+    if (win && win.get_window_type() !== Meta.WindowType.DESKTOP) {
+      return win;
     }
 
-    activate(metawin) {
-        const now = global.get_current_time();
-        metawin.activate(now);
-        metawin.focus(now);
-    }
+    return null;
+  }
 
-    hide(metawin) {
-        metawin.minimize();
+  appIsBound(app) {
+    for (const a of this.apps) {
+      if (a[0] && app.get_id() === a[0].get_id()) {
+        return true;
+      }
     }
+    return false;
+  }
+
+  activate(metawin) {
+    const now = global.get_current_time();
+    metawin.activate(now);
+    metawin.focus(now);
+  }
+
+  hide(metawin) {
+    metawin.minimize();
+  }
 }
